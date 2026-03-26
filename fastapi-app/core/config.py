d586ba7d3bcbbc9bee47610fabd94039383bead7
Grisha-GitaHub <grishavertex@gmail.com>
2026-03-26 21:25:27 +0300

    init: sqlite db

--- fastapi-app/core/config.py
+++ fastapi-app/core/config.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, AnyUrl, TypeAdapter
 from pydantic_settings import BaseSettings
+from pydantic.networks import UrlConstraints
+from typing import Annotated
+
+SqliteUrl = Annotated[
+    AnyUrl,
+    UrlConstraints(allowed_schemas=["sqlite", "sqlite+aiosqlite"])
+]
+
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -8,9 +16,11 @@ class RunConfig(BaseModel):
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
  
-
+class DatabaseConfig(BaseModel):
+    url: SqliteUrl
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 settings = Settings()
