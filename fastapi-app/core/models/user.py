45f601baca97ba32e281cff0c010528cc466cdd4
Grisha-GitaHub <grishavertex@gmail.com>
2026-03-27 19:41:07 +0300

    updare user table

--- fastapi-app/core/models/user.py
+++ fastapi-app/core/models/user.py
@@ -1,7 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
-
+from pydantic import EmailStr
 
 from .base import Base
 
 class User(Base):
     username: Mapped[str] 
+    userfamily: Mapped[str]
+    userpatronic: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str]
