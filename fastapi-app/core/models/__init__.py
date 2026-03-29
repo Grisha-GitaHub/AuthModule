6f9028b98af307028547022ed216eecdb32c1af1
Grisha-GitaHub <grishavertex@gmail.com>
2026-03-29 18:38:35 +0300

    update init

--- fastapi-app/core/models/__init__.py
+++ fastapi-app/core/models/__init__.py
@@ -1,9 +1,24 @@
 __all__ = (
     "db_helper",
-    "Base"
+    "Base",
     "User",
+    "Role",
+    "Permission",
+    "RolePermission",
+    "UserRole",
+    "TokenBlacklist",
+    "user_has_permission",
+    "get_user_permissions",
+    "get_user_roles",
 )
 
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .role import Role
+from .permission import Permission
+from .role_permission import RolePermission
+from .user_role import UserRole
+from .token_blacklist import TokenBlacklist
+
+from .access_control import get_user_permissions, get_user_roles, user_has_permission
