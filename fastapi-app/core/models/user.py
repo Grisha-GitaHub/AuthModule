c88fb967db3f5490a262f95f8fc94d2e6130e812
Grisha-GitaHub <grishavertex@gmail.com>
2026-03-29 19:16:30 +0300

    update user core

--- fastapi-app/core/models/user.py
+++ fastapi-app/core/models/user.py
@@ -1,12 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from pydantic import EmailStr
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Boolean, String
 
 from .base import Base
 
+
 class User(Base):
-    username: Mapped[str] 
-    userfamily: Mapped[str]
-    userpatronic: Mapped[str]
-    email: Mapped[str] = mapped_column(unique=True)
-    hashed_password: Mapped[str]
-    is_active: Mapped[bool] 
+    username: Mapped[str] = mapped_column(String(50))
+    userfamily: Mapped[str] = mapped_column(String(50))
+    userpatronic: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
+    hashed_password: Mapped[bytes] = mapped_column(String(255))
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, server_default="true")
+
+    # Связи
+    roles = relationship("UserRole", back_populates="user", lazy="selectin", cascade="all, delete-orphan")
+
+    def __repr__(self) -> str:
+        return f"<User {self.email}>"  
